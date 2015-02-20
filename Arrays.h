46785200669578681abe11036bcc187327c2747f
VincentCZ <VincentCZ@users.noreply.github.com>
2015-02-20 21:06:05 +0800

    Create Arrays.h
    Chapter 1 c header files

---
+++ Arrays.h
@@ -0,0 +1,14 @@
+/*
+ * Arrays.h
+ *
+ *  Created on: 2015年2月13日
+ *      Author: Leon
+ */
+
+#ifndef ARRAYS_H_
+#define ARRAYS_H_
+
+void reverse(char *);
+int replaceSpaces(char*,int);
+
+#endif /* ARRAYS_H_ */
