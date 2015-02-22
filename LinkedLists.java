6cc9abc30551b5b8516d878cefa03b7ff67c7476
VincentC <chenleii@live.cn>
2015-02-23 00:44:37 +0800

    Rename Class LinkedList to LinkedLists
    Delete LinkedList.java
    Add LinkedLists.java

---
+++ LinkedLists.java
@@ -0,0 +1,71 @@
+
+import java.util.Hashtable;
+
+public class LinkedLists {
+	
+	/* 
+	 * 2.1	Write code to remove duplicates from an unsorted linked list.
+	 * 		FOLLOW UPt
+	 * 		How would you solve this problem if a temporary buffer is not allowed?
+	 */	
+	public static void deleteDups(LinkedListNode n) {
+		Hashtable<Object, Boolean> table = new Hashtable<Object, Boolean>();
+		LinkedListNode previous = null;
+		
+		while(n != null) {
+			if (table.containsKey(n.object)) {
+				previous.next = n.next;
+			} else {
+				table.put(n.object,true);
+				previous = n;
+			}
+			n = n.next;
+		}
+	}
+	/*	without buffer	 */
+	public static void deleteDups2(LinkedListNode head) {
+		if (head == null) return;
+		
+		LinkedListNode current = head;
+		while (current != null) {
+			LinkedListNode runner = current;
+			while(runner.next != null) {
+				if (runner.next.object == current.object) {
+					runner.next = runner.next.next;
+				} else {
+					runner = runner.next;
+				}
+			}
+			current = current.next;
+		}
+	}
+	/*
+	 * 	2.2 Implement an algorithm to find the kth to last element of
+	 * 	 a singly linked list.
+	 */
+	/*Approach A: Don't Return the Element*/
+	public static int nthToLast(LinkedListNode head, int k) {
+		if (head == null) {
+			return 0;
+		}
+		int i = nthToLast(head.next, k) + 1;
+		if (i == k) {
+			System.out.println(head.object);
+		}
+		return i;
+	}
+	/*Approach C: Create a Wrapper Class*/	
+	public static LinkedListNode nthToLastR2(LinkedListNode head, int k, IntWrapper i) {
+		if (head == null) {
+			return null;
+		}
+		LinkedListNode node = nthToLastR2(head.next, k, i);
+		i.value = i.value + 1;
+		if (i.value == k) {
+			return head;
+		}
+		return node;
+	}
+	
+}
+
